f9f8ae40d48d85442860410837bd76f7f04462a4
Dan Sazonov <p.294803@gmail.com>
2023-05-31 06:29:55 +0300

    Картинка на главном кадре индекса

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 //= components/script.js
+
+
+const windowHeight = $(window).height();
+let previewPhotoHeight = windowHeight - $("header").height();
+
+$(".preview_photo").height(previewPhotoHeight);
+
